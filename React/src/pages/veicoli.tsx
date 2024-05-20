e5247ec6310d386672558e4b211ffdb89eadce24
Manuel393 <manuel393.sciuto@gmail.com>
2024-05-20 12:02:51 +0200

    Updates Uni 20/05/2024

--- React/src/pages/veicoli.tsx
+++ React/src/pages/veicoli.tsx
@@ -1,66 +1,109 @@
 import Logo3 from "../components/svgs/logo3.tsx";
-import {validateLogin} from "../components/validation.ts";
-import {useEffect, useState} from "react";
+import { validateLogin } from "../components/validation.ts";
+import { useEffect, useState } from "react";
 import NoUserCountdown from "../components/noUserCountdown.tsx";
 import ProfiloVeicolo from "../components/profiloVeicolo.tsx";
+import { twMerge } from "tailwind-merge";
+import { sleep } from "../components/sleep.ts";
 
 function Veicoli() {
+  const [userId, setUserId] = useState(-1);
+  const [isEmployee, setIsEmployee] = useState<null | boolean>(null);
+  const [error, setError] = useState("");
+  const [vehiclesNumber, setVehiclesNumber] = useState(0);
 
-    const [userId, setUserId] = useState(-1);
-    const [isEmployee, setIsEmployee] = useState<null | boolean>(null);
+  const handleError = async (errorText: string) => {
+    setError(errorText);
+    await sleep(4000);
+    setError("");
+  };
 
-    async function setAuth() {
-        const id = await validateLogin();
-        if (id) setUserId(id);
-    }
+  async function setAuth() {
+    const id = await validateLogin();
+    if (id) setUserId(id);
+  }
 
-    async function setEmployeeBool() {
-        const req = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({id: userId}),
-        };
-        const response = await fetch('http://localhost:8000/users/is_employee.php', req);
-        if (response.ok) {
-            const responseData = await response.text();
-            console.log(responseData)
-            if (responseData === "Utente non trovato") {
-                setIsEmployee(null);
-            } else {
-                setIsEmployee(responseData === "true");
-            }
-        } else setIsEmployee(null);
-    }
-
-    useEffect(() => {
-        setAuth().then(null);
-    }, []);
-
-    useEffect(() => {
-        setEmployeeBool().then(null);
-    }, [userId]);
-
-    if (userId === -1 || isEmployee === null) return <NoUserCountdown/>
-
-    return (
-        <div>
-            <div className="flex w-fit mx-auto mt-4 justify-center items-center gap-x-2 select-none">
-                <Logo3/>
-                <h1 className="h-fit text-3xl uppercase font-lato">I tuoi veicoli</h1>
-                <Logo3/>
-            </div>
-            {isEmployee ?
-                <div className="flex justify-center flex-wrap gap-4 p-4">
-                    <ProfiloVeicolo/>
-                    <ProfiloVeicolo/>
-                    <ProfiloVeicolo/>
-                    <ProfiloVeicolo/>
-                    <ProfiloVeicolo/>
-                </div> :
-                <p>Client</p>
-            }
-        </div>
+  async function setEmployeeBool() {
+    const req = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: userId }),
+    };
+    const response = await fetch(
+      "http://localhost:8000/users/is_employee.php",
+      req,
     );
+    if (response.ok) {
+      const responseData = await response.text();
+      if (responseData === "Utente non trovato") {
+        setIsEmployee(null);
+      } else {
+        setIsEmployee(responseData === "true");
+      }
+    } else setIsEmployee(null);
+  }
+
+  async function getClientNumOfVehicles() {
+    const req = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: userId }),
+    };
+    const response = await fetch(
+      "http://localhost:8000/vehicles/get_user_vehicles_number.php",
+      req,
+    );
+    if (response.ok) {
+      const responseData = await response.text();
+      if (responseData === "Utente non trovato") {
+        setError("Errore, utente non trovato, ricaricare la pagina, grazie.");
+      } else {
+        setVehiclesNumber(parseInt(responseData));
+      }
+    }
+  }
+
+  useEffect(() => {
+    setAuth().then(null);
+  }, []);
+
+  useEffect(() => {
+    setEmployeeBool().then(null);
+    if (!isEmployee && userId !== -1) {
+      getClientNumOfVehicles().then(null);
+    }
+  }, [userId]);
+
+  if (userId === -1 || isEmployee === null) return <NoUserCountdown />;
+
+  return (
+    <div>
+      <div
+        className={twMerge(
+          "bg-red-600 text-white duration-150 font-semibold text-lg",
+          error !== "" && "h-0 px-2 py-1",
+        )}
+      >
+        {error}
+      </div>
+      <div className="flex w-fit mx-auto mt-4 justify-center items-center gap-x-2 select-none">
+        <Logo3 />
+        <h1 className="h-fit text-3xl uppercase font-lato">I tuoi veicoli</h1>
+        <Logo3 />
+      </div>
+      {isEmployee ? (
+        <div className="flex justify-center flex-wrap gap-4 p-4"></div>
+      ) : (
+        <div>
+          {vehiclesNumber < 5 ? (
+            <p>Hai {vehiclesNumber} mezzi registrati</p>
+          ) : (
+            <p>Hai 5 mezzi registati, eliminane uno per aggiungerne altri</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Veicoli;
