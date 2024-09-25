2271934cc5d0c49df7140749198d7d8ed37d0f39
Manuel Rosario Sciuto <manuel393.sciuto@gmail.com>
2024-09-25 19:09:55 +0200

    Ennesime modifiche inutili al proxy nginx

--- React/src/pages/home.tsx
+++ React/src/pages/home.tsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
+
 function Home() {
-  const nomeServizio = process.env.NOME_SERVIZIO || "Unknown Replica";
+  const [numReplica, setNumReplica] = useState("");
+
+  useEffect(() => {
+    setNumReplica(
+      process.env.REACT_APP_REPLICA_NUM
+        ? process.env.REACT_APP_REPLICA_NUM
+        : "Numero replica assente"
+    );
+  });
 
   return (
     <div>
-      <h1>Servizio: {nomeServizio}</h1>
+      <h1>Replica Numero: {numReplica}</h1>
     </div>
   );
 }
